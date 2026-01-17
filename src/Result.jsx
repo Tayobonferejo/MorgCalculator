6b6c58605a1a781e85b6204028596ad13fcb60ca
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-01-17 11:07:55 +0100

    more styling result component

--- src/Result.jsx
+++ src/Result.jsx
@@ -1,8 +1,9 @@
 import empty from "./assets/images/illustration-empty.svg"
+import "./Result.css"
 
 function Result (){
     return(
-        <div>
+        <div className="result-div">
             <img src={empty}></img>
             <h2>Results Shown here</h2>
             <p>Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
